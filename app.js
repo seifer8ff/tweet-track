1b347cb64c354cfda11a3213025399b1c5855ec7
seifer8ff <brady.bcmp@gmail.com>
2017-03-02 13:18:50 -0600

    Added partials + CSS
    Added partials for header and footer, included CSS files and updated
    .gitignore to ignore node_modules

--- app.js
+++ app.js
@@ -5,17 +5,23 @@ var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 var twitter = require('twitter');
 
+// serve files in the public directory
+app.use(express.static("public"));
+// default to ejs templating
 app.set("view engine", "ejs");
 
 var twit = new twitter({
+	// api keys are defined in a config file- excluded from github for privacy
     consumer_key: config.twitter.consumerKey,
     consumer_secret: config.twitter.consumerSecret,
     access_token_key: config.twitter.accessTokenKey,
     access_token_secret: config.twitter.accessTokenSecret
 });
 
+// stream tweets by keyword
 var tweetStream = twit.stream('statuses/filter', { track: 'gay' });
 
+// make streaming tweets available to front end via websockets
 io.on('connect', function(socket) {
 	tweetStream.on('data', function(tweet) {
 		socket.emit('tweets', tweet.text);
@@ -29,6 +35,7 @@ app.get("/", function(req, res){
     res.render("homepage");
 });
 
+// socket.io server rather than expresss
 server.listen(3000, function(){
 	console.log("socker server is listening on port 3000");
 });

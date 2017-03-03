45bca1ac043befc29e4edb054dc032b0b8618ce8
seifer8ff <brady.bcmp@gmail.com>
2017-03-03 17:51:15 -0600

    Moved from paperJS to plot.ly
    Removed references to paperJS and added plot.ly.
    Changed visualizer to a graph and customized graph.

--- public/main.js
+++ public/main.js
@@ -1,9 +1,13 @@
 var socket = io();
+var tweetCountTotal = 0;
+var tweetCountCurrent = 0;
 
 socket.on('connection', function() {
 	console.log('Connected!');
 });
 socket.on('tweets', function(tweet) {
+	tweetCountTotal += 1;
+	tweetCountCurrent += 1;
 	// creates a new h4 for each incoming tweet
 	$(".stream__tweets").prepend("<h4>" + tweet + "</h4>");
 });

5a6dde83c9f3cbb9746835d7a5796fb681ac68a5
seifer8ff <brady.bcmp@gmail.com>
2017-03-11 17:24:14 -0600

    Added mongoldb + refactored functionality
    Changed app to write tweets into database upon streaming from twitter
    and read tweets from database while page is open.

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
-var config = require("./config");
-var express = require("express");
-var app = express();
-var server = require("http").createServer(app);
-var io = require("socket.io")(server);
-var twitter = require('twitter');
+var config = require("./config"),
+	express = require("express"),
+	app = express(),
+	server = require("http").createServer(app),
+	io = require("socket.io")(server),
+	twitter = require('twitter'),
+	bodyParser = require("body-parser"),
+	mongoose = require("mongoose"),
+	queryString = "gay,Clinton";
 
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/twitter-stream");
+// use bodyparser
+app.use(bodyParser.urlencoded({extended: true}));
 // serve files in the public directory
 app.use(express.static("public"));
 // default to ejs templating
 app.set("view engine", "ejs");
+
+
+var tweetSchema = new mongoose.Schema({
+	name: String,
+	body: String,
+	time: String
+}, 
+{
+	capped: {size: 5242880, max: 10000, autoIndexId: true}
+});
+
+var querySchema = new mongoose.Schema({
+	keyword: String,
+	date: String
+});
+
+var Tweet = mongoose.model("tweet", tweetSchema);
+var Query = mongoose.model("query", querySchema);
 
 var twit = new twitter({
 	// api keys are defined in a config file- excluded from github for privacy
@@ -19,23 +45,63 @@ var twit = new twitter({
 });
 
 // stream tweets by keyword
-var tweetStream = twit.stream('statuses/filter', { track: 'gay' });
+var tweetStream = twit.stream('statuses/filter', { track: queryString });
 
-// make streaming tweets available to front end via websockets
-io.on('connect', function(socket) {
-	tweetStream.on('data', function(tweet) {
-		socket.emit('tweets', tweet.text);
-	});
-	tweetStream.on('error', function(error) {
-		throw error;
+tweetStream.on('data', function(tweet) {
+	Tweet.create({
+		name: tweet.user["screen_name"],
+		body: tweet.text,
+		time: tweet.created_at
+	}, function(err, newTweet) {
+		if (err) {
+			console.log(err);
+		}
 	});
 });
+
+tweetStream.on('error', function(error) {
+	throw error;
+});
+
 
 app.get("/", function(req, res){
 	res.render("homepage");
 });
 
+app.post("/", function(req, res) {
+	res.redirect("/");
+});
+
+
+
+// make streaming tweets available to front end via websockets
+io.on('connect', function(socket) {
+	console.log("connected to socket.io");
+});
+
+var dbStream = Tweet.find().tailable(true, {awaitdata: true, numberOfRetries: 500, tailableRetryInterval : 1000}).stream();
+dbStream.on("data", function(dbTweet) {
+	io.emit('tweets', dbTweet.body);
+});
+
+dbStream.on("error", function(err) {
+	console.log(err);
+})
+
+
+
+
+
 // socket.io server rather than expresss
 server.listen(3000, function(){
 	console.log("socker server is listening on port 3000");
 });
+
+
+
+function buildQueryString(newKeyword) {
+	if (newKeyword) {
+		queryString += "," + newKeyword;
+	}
+	return queryString;
+}
